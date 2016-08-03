d2ba25ae4b0d2d5198564df518a1d16200703cb1
simis2626 <Simos2632>
2016-08-03 22:05:14 +1000

    Great

--- routes/upload.js
+++ routes/upload.js
@@ -3,28 +3,32 @@ var router = express.Router();
 var mongo1 = require('mongodb').MongoClient;
 var mongo2 = require('mongodb');
 var fs = require ('fs');
+var multa = require('multer');
+var multa1 = multa({inMemory:true});
 
 
 /* Connect to db. */
-router.use('/', function(req, res, next) {
+
+router.post('/', multa1.single('data2'),function(req, res, next) {
  var url = 'mongodb://10.3.0.47:27017/nodehtml';
+ //console.log(req.file);
+ console.log(req.body);
 
 mongo1.connect(url, function(err, db) {
 
-console.log(err);
+//console.log(err);
 
  var gridfs = new mongo2.GridFSBucket(db);
- console.log(req.filedata.toString());
- fs.createReadStream(req.filedata).pipe(gridfs.openUploadStream('test')).on('finish',function(){
+var buffer1 = Buffer.from(req.file.buffer);
+
+console.log(buffer1);
+
+fs.createReadStream(buffer1).pipe(gridfs.openUploadStream(req.file.originalname)).on('finish', function(){
 
 
- console.log(
-     'made it here'
-
-
- );
  res.render('index',{title:'Upload Successful'});
 });
+
 
 
 
