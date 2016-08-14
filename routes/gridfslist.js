37e566d2c0100d3ee04cb7d0190dcb5f3e4783d2
simis2626 <Simos2632>
2016-08-14 17:03:06 +1000

    Great

--- routes/gridfslist.js
+++ routes/gridfslist.js
@@ -6,7 +6,10 @@ var router = express.Router();
 var mongo1 = require('mongodb').MongoClient;
 var mongo2 = require('mongodb');
 var mongoObject = require('mongodb').ObjectID;
-
+var fs = require ('fs');
+var multa = require('multer');
+var multa1 = multa({inMemory:true});
+var streamer = require('streamifier');
 
 
 router.get('/get/:id', function(req, res, next) {
@@ -57,6 +60,28 @@ router.get('/delete/:id', function(req, res, next) {
 
 
         });
+
+
+router.post('/', multa1.single('data2'),function(req, res, next) {
+        var url = 'mongodb://10.3.0.47:27017/nodehtml';
+        //console.log(req.file);
+        console.log(req.body);
+
+        mongo1.connect(url, function(err, db) {
+
+//console.log(err);
+
+                var gridfs = new mongo2.GridFSBucket(db);
+
+
+                console.log(req.file);
+
+                streamer.createReadStream(req.file.buffer).pipe(gridfs.openUploadStream(req.file.originalname).on('finish', function(){
+
+
+                        res.redirect('back');
+                }));});});
+
 
 
 
