6f00164e6ea95c46474dd67ad3f0f711ea5d6331
simis2626 <Simos2632>
2016-08-14 10:16:23 +1000

    Great

--- routes/gridfslist.js
+++ routes/gridfslist.js
@@ -4,6 +4,26 @@
 var express = require('express');
 var router = express.Router();
 var mongo1 = require('mongodb').MongoClient;
+var mongo2 = require('mongodb');
+var mongoObject = require('mongodb').ObjectID;
+
+
+
+router.get('/get/:id', function(req, res, next) {
+        var url = 'mongodb://10.3.0.47:27017/nodehtml';
+
+        mongo1.connect(url, function(err, db) {
+
+                var findObjectId = new mongoObject.ObjectID(req.params.id);
+                console.log(findObjectId);
+
+                var gridfs = new mongo2.GridFSBucket(db);
+                gridfs.openDownloadStream(findObjectId).pipe(res);
+
+
+
+        })});
+
 
 
 /* Connect to db. */
@@ -16,7 +36,11 @@ router.get('/', function(req, res, next) {
                 console.log("Connected Successfully");
 
                 db.collection('fs.files').find().toArray(function(err, docs){
-                        docs.forEach(id="/get"+_id)
+                        for( var yt=0; yt<docs.length; yt++){
+                                docs[yt]._id = "/gridfslist/get/" + docs[yt]._id;
+
+
+                        }
                         res.render('gridfslist',{title:'Stored Files', DB:docs});
 
 
