b2b841fbed36b0da026906a263e1bf86d2904cf8
andromeda <g.simon.levett@gmail.com>
2016-08-18 22:21:53 +1000

    working root component

--- marketing-app/root/app.component.ts
+++ marketing-app/root/app.component.ts
@@ -1,10 +1,57 @@
 /**
  * Created by andromeda on 18/08/2016.
  */
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 @Component({
     selector: 'root-component',
-    template: '<h1>My First Angular 2 App</h1>'
+    templateUrl: 'app/root/app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+    selectedHeader;
+    activeClass = "btn btn-primary active";
+    inactiveClass = "btn btn-default";
+    headerButtons;
+
+    ngOnInit() {
+        this.headerButtons = [
+            this.activeClass, this.inactiveClass, this.inactiveClass
+
+        ];
+    }
+
+
+    headingClicked(inString: string) {
+
+        this.selectedHeader = inString;
+        switch (this.selectedHeader) {
+
+            case "marketing":
+                this.headerButtons = [
+                    this.activeClass, this.inactiveClass, this.inactiveClass
+
+                ];
+                break;
+            case "consultant":
+                this.headerButtons = [
+                    this.inactiveClass, this.activeClass, this.inactiveClass
+
+                ];
+                break;
+            case "seeker":
+                this.headerButtons = [
+                    this.inactiveClass, this.inactiveClass, this.activeClass
+
+                ];
+                break;
+        }
+
+
+    }
+
 }
+
+
+
+
+
