726e82caf362feeb2b6c3a1be831fc4e18048e14
andromeda <g.simon.levett@gmail.com>
2016-08-18 21:06:25 +1000

    working root component

---
+++ marketing-app/main.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by andromeda on 18/08/2016.
+ */
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {AppModule} from './root/app.module';
+platformBrowserDynamic().bootstrapModule(AppModule);
---
+++ marketing-app/root/app.component.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by andromeda on 18/08/2016.
+ */
+import {Component} from '@angular/core';
+@Component({
+    selector: 'root-component',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+export class AppComponent {
+}
