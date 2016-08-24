5aff35529d2e962a3e3d9090ecdd9db23347dc35
root <root@node-server>
2016-08-24 21:46:53 +1000

    gshsjjsjs

--- install-script.sh
+++ install-script.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 systemctl stop node-server
+mv first-trial/node_modules /root/
 rm -r first-trial
 echo -n "Enter the branch name > "
 read text
 git clone --branch $text https://github.com/simis2626/first-trial.git
+mv /root/node_modules first-trial/
 cd first-trial
-apt-get update
-apt-get install
-apt-get upgrade
 npm install
 cd ..
 chmod -R 777 *
 cd first-trial
+npm run postinstall
 npm run tsc
+sed -i 's/3000/80/g' bin/www
 systemctl start node-server
 echo "Process Complete"
