89259965b19b994f49931bc8f2416269cac6bc0f
simis2626 <Simos2632>
2016-09-02 17:17:43 +1000

    https

--- marketing-app/root/app.component.ts
+++ marketing-app/root/app.component.ts
@@ -4,13 +4,17 @@
 
 import {Component} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import {AuthService} from '../auth0/auth.service';
+
 
 
 @Component({
     selector: 'root-component',
     templateUrl: 'app/root/app.component.html'
+
 })
 export class AppComponent {
-
+    constructor(private authProvider: AuthService) {
+    }
 
 }
