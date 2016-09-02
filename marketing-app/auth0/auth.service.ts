2816baa011045e1e0e1321bdf2ba9a795e37ee5d
simis2626 <Simos2632>
2016-09-02 17:56:56 +1000

    https

--- marketing-app/auth0/auth.service.ts
+++ marketing-app/auth0/auth.service.ts
@@ -3,6 +3,8 @@
  */
 import {Injectable}      from '@angular/core';
 import {tokenNotExpired} from 'angular2-jwt';
+import {Auth0Lock} from 'auth0-lock';
+
 
 // Avoid name not found warnings
 declare var Auth0Lock: any;
