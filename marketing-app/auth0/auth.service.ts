6d142c581b91d13a4c238493c1f35ad0814b003b
simis2626 <Simos2632>
2016-09-02 17:39:20 +1000

    https

--- marketing-app/auth0/auth.service.ts
+++ marketing-app/auth0/auth.service.ts
@@ -2,7 +2,7 @@
  * Created by Andromeda on 2/09/2016.
  */
 import {Injectable}      from '@angular/core';
-import {tokenNotExpired} from '/node_modules/angular2-jwt';
+import {tokenNotExpired} from 'angular2-jwt';
 
 // Avoid name not found warnings
 declare var Auth0Lock: any;
