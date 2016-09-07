ac16593bd8b3244098da166db09b6e1509dc1659
simis2626 <Simos2632>
2016-09-08 06:13:48 +1000

    working post

---
+++ marketing-app/small-components/confirm-button.component.ts
@@ -0,0 +1,46 @@
+/**
+ * Created by Andromeda on 7/09/2016.
+ */
+
+import {Component, Output, EventEmitter} from '@angular/core';
+
+
+@Component({
+    styleUrl: ['app/small-components/confirm-button.css'],
+    templateUrl: 'app/small-components/confirm-button.component.html',
+
+    selector: 'confirm-button'
+
+
+})
+export class ConfirmButtonComponent {
+
+    @Output() clickConfirmed = new EventEmitter();
+
+    confirming: boolean;
+    confirmingWrapper: boolean;
+    clicked: boolean;
+
+
+    parentClicked() {
+        this.transition(true);
+    }
+
+
+    cancelClicked() {
+        this.transition(false);
+    }
+
+
+    continueClicked() {
+        this.clickConfirmed.emit();
+
+    }
+
+    transition(selector: boolean) {
+
+
+    }
+
+
+}
